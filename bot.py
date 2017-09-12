3c417d422231747de1c784d8cc1ad35b2a173c68
Vlad Utkin <vlad@teamfnd.ru>
2017-09-13 00:13:09 +0300

    Мелкие косметические правки.

--- bot.py
+++ bot.py
@@ -30,4 +30,6 @@ def processing():
 def hello():
     return "HELLO"
 
-app.run(host='0.0.0.0', debug=True)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
--- routerHandler.py
+++ routerHandler.py
@@ -3,6 +3,7 @@
 from bot_session import getRoute
 from routes import *
 
+
 def route(user_id, body):
     rout = getRoute(user_id)
     return routes[rout](user_id, body)
